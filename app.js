ab1cc41c9d732659ae63fb28d60b5b1594ff7e75
Nash-Aud <92648695+Nash-Aud@users.noreply.github.com>
2023-05-16 10:14:03 -0400

    added functionalities

--- app.js
+++ app.js
@@ -5,15 +5,28 @@ document.addEventListener("DOMContentLoaded", function() {
     const inputEmail = document.querySelector("#email");
     const inputAsunto = document.querySelector("#asunto");
     const inputMensaje = document.querySelector("#mensaje");
+    const formulario = document.querySelector("#formulario");
 
     //Asignar Eventos
-    inputEmail.addEventListener("blur", function(e) {
-        console.log(e.target.value);
-    });
-    inputAsunto.addEventListener("blur", function(e) {
-        console.log(e.target.value);
-    });
-    inputMensaje.addEventListener("blur", function(e) {
-        console.log(e.target.value);
-    });
+    inputEmail.addEventListener("blur", validar);
+    inputAsunto.addEventListener("blur", validar);
+    inputMensaje.addEventListener("blur", validar);
+
+    function validar(e) {
+        if(e.target.value.trim() === "") {
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`);
+        } else {
+            console.log("Hay algo");
+        }
+    }
+
+    function mostrarAlerta(mensaje) {
+        //Generar alerta
+        const error = document.createElement("P");
+        error.textContent = mensaje;
+        error.classList.add("bg-red-600", "text-white", "p-2", "text-center");
+
+        //Injectar error al formulario
+        formulario.appendChild(error);
+    }
 });
