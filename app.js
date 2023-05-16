fd7ee3783b749c59dd2f99d6172fa5feb771fbd5
Nash-Aud <92648695+Nash-Aud@users.noreply.github.com>
2023-05-16 16:45:10 -0400

    project finished

--- app.js
+++ app.js
@@ -1,95 +1,124 @@
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
 
     const email = {
-        email: "",
-        asunto: "",
-        mensaje: ""
+        email: '',
+        asunto: '',
+        mensaje: ''
     }
 
-    //Seleccionar elementos de la interfaz
-    const inputEmail = document.querySelector("#email");
-    const inputAsunto = document.querySelector("#asunto");
-    const inputMensaje = document.querySelector("#mensaje");
-    const formulario = document.querySelector("#formulario");
-    const btnSubmit = document.querySelector("#formulario button[type='submit']");
-    const btnReset = document.querySelector("#formulario button[type='reset']");
+    // Seleccionar los elementos de la interfaz
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spinner = document.querySelector('#spinner');
 
-    //Asignar Eventos
-    inputEmail.addEventListener("input", validar);
-    inputAsunto.addEventListener("input", validar);
-    inputMensaje.addEventListener("input", validar);
+    // Asignar eventos
+    inputEmail.addEventListener('input', validar);
+    inputAsunto.addEventListener('input', validar);
+    inputMensaje.addEventListener('input', validar);
 
-    btnReset.addEventListener("click", function(e) {
+    formulario.addEventListener('submit', enviarEmail);
+
+    btnReset.addEventListener('click', function(e) {
+        e.preventDefault();
+        resetFormulario();
+    })
+
+    function enviarEmail(e) {
         e.preventDefault();
 
-        //reiniciar objeto
-        email.email = "",
-        asunto.asunto = "",
-        mensaje.mensaje = "";
+        spinner.classList.add('flex');
+        spinner.classList.remove('hidden');
 
-        formulario.reset();
-        comprobarEmail();
-    });
+        setTimeout(() => {
+            spinner.classList.remove('flex');
+            spinner.classList.add('hidden');
+
+            resetFormulario();
+
+            // Crear una alerta
+            const alertaExito = document.createElement('P');
+            alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-10', 'font-bold', 'text-sm', 'uppercase');
+            alertaExito.textContent = 'Mensaje enviado correctamente';
+
+            formulario.appendChild(alertaExito);
+
+            setTimeout(() => {
+                alertaExito.remove(); 
+            }, 3000);
+        }, 3000);
+    }
 
     function validar(e) {
-        if(e.target.value.trim() === "") {
-            mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
-            email[e.target.name] = "";
+        if(e.target.value.trim() === '') {
+            mostrarAlerta(`El Campo ${e.target.id} es obligatorio`, e.target.parentElement);
+            email[e.target.name] = '';
             comprobarEmail();
             return;
-        } 
+        }
 
-        if(e.target.id === "email" && !validarEmail(e.target.value)) {
-            mostrarAlerta("El email no es valido", e.target.parentElement);
-            email[e.target.name] = "";
+        if(e.target.id === 'email' && !validarEmail(e.target.value)) {
+            mostrarAlerta('El email no es válido', e.target.parentElement);
+            email[e.target.name] = '';
             comprobarEmail();
             return;
-        };
+        }
 
         limpiarAlerta(e.target.parentElement);
 
-        //Asignar los valores
+        // Asignar los valores
         email[e.target.name] = e.target.value.trim().toLowerCase();
 
-        //Comprobar el objeto de email
+        // Comprobar el objeto de email
         comprobarEmail();
     }
 
     function mostrarAlerta(mensaje, referencia) {
         limpiarAlerta(referencia);
-
-        //Generar alerta
-        const error = document.createElement("P");
+        
+        // Generar alerta en HTML
+        const error = document.createElement('P');
         error.textContent = mensaje;
-        error.classList.add("bg-red-600", "text-white", "p-2", "text-center");
-
-        //Injectar error al formulario
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
+       
+        // Inyectar el error al formulario
         referencia.appendChild(error);
     }
 
     function limpiarAlerta(referencia) {
-        //comprueba si ya existe una alerta
-        const alerta = referencia.querySelector(".bg-red-600");
+        // Comprueba si ya existe una alerta
+        const alerta = referencia.querySelector('.bg-red-600');
         if(alerta) {
             alerta.remove();
         }
     }
 
     function validarEmail(email) {
-        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
         const resultado = regex.test(email);
         return resultado;
     }
 
     function comprobarEmail() {
-        if(Object.values(email).includes("")) {
-            btnSubmit.classList.add("opacity-50");
-            btnSubmit.diabled = true;
-            return;
+        if(Object.values(email).includes('')) {
+            btnSubmit.classList.add('opacity-50');
+            btnSubmit.disabled = true;
+            return
         } 
-        btnSubmit.classList.remove("opacity-50");
-        btnSubmit.diabled = false;
+        btnSubmit.classList.remove('opacity-50');
+        btnSubmit.disabled = false;
     }
 
+    function resetFormulario() {
+        // reiniciar el objeto
+        email.email = '';
+        email.asunto = '';
+        email.mensaje = '';
+
+        formulario.reset();
+        comprobarEmail();
+    }
 });
